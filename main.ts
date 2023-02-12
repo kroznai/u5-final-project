b6021bdc01b55987e11c4c0cec6862cd3adfada4
Brett Rich <112567108+B4-Bingo@users.noreply.github.com>
2023-02-12 14:53:57 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,11 @@
 // Make sure to read all the requirements & add comments to your code!
-game.splash("Hello, World!")
+let list = [
+4,
+3,
+1,
+2,
+0
+]
+for (let value of list) {
+    game.splash("Hello " + "World " + value)
+}
