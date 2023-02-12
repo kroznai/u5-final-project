af06827005f28bfbd71270c4c69be8d519049314
Brett Rich <112567108+B4-Bingo@users.noreply.github.com>
2023-02-12 15:41:57 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 // Make sure to read all the requirements & add comments to your code!
+let Total = 0
 for (let index = 0; index <= 4; index++) {
-    game.splash("Goodbye" + "World" + index)
+    Total = index + index
+    game.splash("Goodbye" + "World" + Total)
 }
