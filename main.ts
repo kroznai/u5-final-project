a23041a412023fae3350519554dbd3939bb79d12
kroznai <53953781+kroznai@users.noreply.github.com>
2023-02-12 22:25:06 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,1 @@
 // Make sure to read all the requirements & add comments to your code!
-for (let index = 0; index <= 4; index++) {
-    game.showLongText("Hello" + "again" + index, DialogLayout.Bottom)
-}
